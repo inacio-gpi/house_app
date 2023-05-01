9e04d3671e651c6a57c79bee71bfce172757f014
Guilherme Inacio <okkopsiu@gmail.com>
2023-05-01 11:42:59 -0300

    added dio options and handleHeader validator

--- lib/core/http/rest_client.dart
+++ lib/core/http/rest_client.dart
@@ -1,11 +1,15 @@
 import 'package:dio/dio.dart';
-import 'package:house_app/core/error/failure.dart';
-import 'package:house_app/core/http/i_rest_client.dart';
+import 'package:house_app/lib.dart';
 
 class RestClient implements IRestClient {
   final Dio _http;
 
-  RestClient(this._http);
+  RestClient(this._http) {
+    _http.options.connectTimeout = const Duration(seconds: 20);
+    _http.options.receiveTimeout = const Duration(seconds: 20);
+    _http.options.responseType = ResponseType.json;
+    _http.options.contentType = Headers.jsonContentType;
+  }
 
   @override
   Future<Response> delete(
@@ -13,32 +17,36 @@ class RestClient implements IRestClient {
     Map<String, dynamic>? body,
     Map<String, dynamic>? header,
     Map<String, dynamic>? queryParams,
-  }) async =>
-      await _executeRequest(
-        _http.delete(
-          path,
-          queryParameters: queryParams,
-          options: Options(
-            headers: header,
-          ),
+  }) async {
+    _http.options.headers = _setHeaders(header);
+    return await _executeRequest(
+      _http.delete(
+        path,
+        queryParameters: queryParams,
+        options: Options(
+          headers: header,
         ),
-      );
+      ),
+    );
+  }
 
   @override
   Future<Response> get(
     String path, {
     Map<String, dynamic>? header,
     Map<String, dynamic>? queryParams,
-  }) async =>
-      await _executeRequest(
-        _http.get(
-          path,
-          queryParameters: queryParams,
-          options: Options(
-            headers: header,
-          ),
+  }) async {
+    _http.options.headers = _setHeaders(header);
+    return await _executeRequest(
+      _http.get(
+        path,
+        queryParameters: queryParams,
+        options: Options(
+          headers: header,
         ),
-      );
+      ),
+    );
+  }
 
   @override
   Future<Response> patch(
@@ -46,16 +54,18 @@ class RestClient implements IRestClient {
     Map<String, dynamic>? body,
     Map<String, dynamic>? header,
     Map<String, dynamic>? queryParams,
-  }) async =>
-      await _executeRequest(
-        _http.patch(
-          path,
-          queryParameters: queryParams,
-          options: Options(
-            headers: header,
-          ),
+  }) async {
+    _http.options.headers = _setHeaders(header);
+    return await _executeRequest(
+      _http.patch(
+        path,
+        queryParameters: queryParams,
+        options: Options(
+          headers: header,
         ),
-      );
+      ),
+    );
+  }
 
   @override
   Future<Response> post(
@@ -63,16 +73,18 @@ class RestClient implements IRestClient {
     body,
     Map<String, dynamic>? header,
     Map<String, dynamic>? queryParams,
-  }) async =>
-      await _executeRequest(
-        _http.post(
-          path,
-          queryParameters: queryParams,
-          options: Options(
-            headers: header,
-          ),
+  }) async {
+    _http.options.headers = _setHeaders(header);
+    return await _executeRequest(
+      _http.post(
+        path,
+        queryParameters: queryParams,
+        options: Options(
+          headers: header,
         ),
-      );
+      ),
+    );
+  }
 
   @override
   Future<Response> put(
@@ -80,22 +92,41 @@ class RestClient implements IRestClient {
     body,
     Map<String, dynamic>? header,
     Map<String, dynamic>? queryParams,
-  }) async =>
-      await _executeRequest(
-        _http.get(
-          path,
-          queryParameters: queryParams,
-          options: Options(
-            headers: header,
-          ),
-        ),
-      );
+  }) async {
+    _http.options.headers = _setHeaders(header);
+    return await _executeRequest(
+      _http.get(
+        path,
+        queryParameters: queryParams,
+      ),
+    );
+  }
 
   Future<Response> _executeRequest(Future<Response> action) async {
     try {
       return await action;
     } catch (error, stackTrace) {
-      throw NetworkException(message: 'Erro ao obter dados da API', stackTrace: stackTrace);
+      throw NetworkException(message: error.toString(), stackTrace: stackTrace);
     }
+  }
+
+  Map<String, dynamic> _setHeaders(Map<String, dynamic>? headers) {
+    String? appToken;
+    if (I.isRegistered<UserService>()) {
+      appToken = I.get<UserService>().currentUser.token;
+    }
+
+    if (headers != null && !headers.containsKey('Authorization')) {
+      headers['Authorization'] = 'Bearer $appToken';
+    }
+
+    final Map<String, dynamic> headerAuthorization;
+    if (appToken != null) {
+      headerAuthorization = {'Authorization': 'Bearer $appToken'};
+    } else {
+      headerAuthorization = {};
+    }
+
+    return headers ?? headerAuthorization;
   }
 }
